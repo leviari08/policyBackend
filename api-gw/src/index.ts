5626815ec46492ca7415f2ffd17ccd8299554b44
Ariel levitas <leviari08@gmail.com>
2023-07-11 13:18:30 +0300

    feat(api-gw): add rate limit

--- api-gw/src/index.ts
+++ api-gw/src/index.ts
@@ -3,6 +3,7 @@ import express, { Application, Request, Response } from 'express';
 import morgan from 'morgan';
 import policyRouter from './routes/policy';
 import validationRouter from './routes/validation';
+import rateLimit from 'express-rate-limit'
 
 // config 
 dotenv.config();
@@ -10,8 +11,17 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-// logger
+const limiter = rateLimit({
+	windowMs: 60 * 1000, // 1 minute
+	max: 10, // Limit each IP to 100 requests per `window` (here, per 1 minute)
+    message: "Rate limit exceeded",
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// middleware
 app.use(morgan('dev'));
+app.use(limiter);
 
 // routes
 app.use(policyRouter);
